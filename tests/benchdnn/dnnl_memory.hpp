29e585a7e16f997f3004a6dc00eb581ef268f333
Maria Zhukova <maria.zhukova@intel.com>
2026-04-22 13:38:18 -0700

    tests: benchdnn: extend handle_info_t to work with multiple ptrs

--- tests/benchdnn/dnnl_memory.hpp
+++ tests/benchdnn/dnnl_memory.hpp
@@ -31,12 +31,14 @@
 struct dnn_mem_t {
     struct handle_info_t {
         bool is_host_ptr;
-        void *ptr;
+        std::vector<void *> ptrs;
 
-        bool is_allocate() const { return ptr == DNNL_MEMORY_ALLOCATE; }
+        bool is_allocate() const {
+            return !ptrs.empty() && ptrs[0] == DNNL_MEMORY_ALLOCATE;
+        }
 
         static handle_info_t allocate() {
-            return {false, DNNL_MEMORY_ALLOCATE};
+            return {false, {DNNL_MEMORY_ALLOCATE}};
         }
     };
 
@@ -178,8 +180,8 @@ struct dnn_mem_t {
     int gpu_fill_random(size_t size, int buffer_index) const;
 #endif
 
-    static dnn_mem_t create_from_host_ptr(
-            const dnnl_memory_desc_t &md, dnnl_engine_t engine, void *host_ptr);
+    static dnn_mem_t create_from_host_ptr(const dnnl_memory_desc_t &md,
+            dnnl_engine_t engine, const dnn_mem_t &mem);
 
     // Increases memory size to catch potential buffer overreads and
     // overwrites. The padded area is filled with a canary value.
